fe0a8fd9537f37e3dc171afc576247510cd918ad
Paul Kuyer <paul.kuyer@tuta.io>
2022-12-24 19:40:27 +0000

    set up config and added directories src, dist and added h1

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function component() {
+	const element = document.createElement("div");
+	const h1 = document.createElement("h1");
+	h1.innerHTML = "toDay";
+
+	element.appendChild(h1);
+	return element;
+}
+
+document.body.appendChild(component());
